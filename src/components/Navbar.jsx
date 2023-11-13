448bb8cf461200d9da6a57ff595921459865fd37
Bruno Capobianco <bruno.capobianco@outlook.it>
2023-11-13 13:59:14 +0100

    some bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,58 @@
 import Container from 'react-bootstrap/Container';
+import { Col } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import { BellFill, Briefcase, ChatDots, Grid3x3, Grid3x3Gap, HouseAddFill, PeopleFill, PersonCircle } from 'react-bootstrap-icons';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 function Navbarl() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home"><img src="assets/174857.png" alt="" width="40" height="40" /></Navbar.Brand>
+        <Form className="d-flex">
+            <Form.Control
+            
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+           
+          </Form>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+          <Nav className="me-auto mx-5">
+            <Col className='mx-2'>
+            <Nav.Link href="#features"> <HouseAddFill className='fs-5'/> </Nav.Link>
+            <span>Home</span>
+            </Col>
+            <Col className='mx-2'>
+            <Nav.Link href="#pricing"> <PeopleFill className='fs-5 '/> </Nav.Link>
+            <span>Rete</span>
+            </Col>
+            <Col className='mx-2'>
+            <Nav.Link href="#pricing"> <Briefcase className='fs-5 '/> </Nav.Link>
+            <span>Lavoro</span>
+            </Col>
+            <Col className='mx-2'>
+            <Nav.Link href="#pricing"> <ChatDots className='fs-5 '/> </Nav.Link>
+            <span>Messaggistica</span>
+            </Col>
+            <Col className='mx-2'>
+            <Nav.Link href="#pricing"> <BellFill className='fs-5 '/> </Nav.Link>
+            <span>Notifiche</span>
+            </Col>
+            <Col className='mx-2'>
+            <Nav.Link href="#pricing"> <PersonCircle className='fs-5 '/> </Nav.Link>
+            <span>Tu</span>
+            </Col>
+         <Col>
+         <Grid3x3Gap className='fs-5 mt-2'/>
+            <NavDropdown title="Per le aziende"  id="collapsible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -24,14 +63,11 @@ function Navbarl() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
+        </Col>
           </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
+          
         </Navbar.Collapse>
+       
       </Container>
     </Navbar>
   );
