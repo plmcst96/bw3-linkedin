c0a53300a9d35d94adfe848ab62f60ceb228e9ce
Bruno Capobianco <bruno.capobianco@outlook.it>
2023-11-13 15:17:50 +0100

    somw bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,21 @@
 import Container from 'react-bootstrap/Container';
-import { Col } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
-import { BellFill, Briefcase, ChatDots, Grid3x3Gap, HouseAddFill, PeopleFill, PersonCircle } from 'react-bootstrap-icons';
+import { BellFill, Briefcase, ChatDots, Grid3x3Gap, HouseAddFill, PeopleFill, PersonCircle, Search } from 'react-bootstrap-icons';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Navbarl() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
+      <Container className='d-flex align-items-center'>
         <Navbar.Brand href="#home"><img src="assets/174857.png" alt="" width="40" height="40" /></Navbar.Brand>
-        <Form className="d-flex">
+        <Search className='d-inline d-lg-none'/>
+
+        <Form className="d-flex d-none d-lg-inline">
             <Form.Control
             
               type="search"
@@ -23,50 +25,41 @@ function Navbarl() {
             />
            
           </Form>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto mx-5">
-            <Col className='mx-2'>
-            <Nav.Link href="#features"> <HouseAddFill className='fs-5'/> </Nav.Link>
-            <span>Home</span>
+        <Nav className="me-auto mx-3">
+         <Row className='align-items-center'>
+            <Col >
+              <Nav.Link href="#features"> <HouseAddFill /> </Nav.Link>
+              <span className='d-none d-md-inline'>Home</span>
             </Col>
-            <Col className='mx-2'>
-            <Nav.Link href="#pricing"> <PeopleFill className='fs-5 '/> </Nav.Link>
-            <span>Rete</span>
+            <Col >
+              <Nav.Link href="#pricing"> <PeopleFill /> </Nav.Link>
+              <span className='d-none d-md-inline'>Rete</span>
             </Col>
-            <Col className='mx-2'>
-            <Nav.Link href="#pricing"> <Briefcase className='fs-5 '/> </Nav.Link>
-            <span>Lavoro</span>
+            <Col >
+              <Nav.Link href="#pricing"> <Briefcase /> </Nav.Link>
+              <span className='d-none d-md-inline'>Lavoro</span>
             </Col>
-            <Col className='mx-2'>
-            <Nav.Link href="#pricing"> <ChatDots className='fs-5 '/> </Nav.Link>
-            <span>Messaggistica</span>
+            <Col >
+              <Nav.Link href="#pricing"> <ChatDots /> </Nav.Link>
+              <span className='d-none d-md-inline'>Messaggistica</span>
             </Col>
-            <Col className='mx-2'>
-            <Nav.Link href="#pricing"> <BellFill className='fs-5 '/> </Nav.Link>
-            <span>Notifiche</span>
+            <Col >
+              <Nav.Link href="#pricing"> <BellFill /> </Nav.Link>
+              <span className='d-none d-md-inline'>Notifiche</span>
             </Col>
-            <Col className='mx-2'>
-            <Nav.Link href="#pricing"> <PersonCircle className='fs-5 '/> </Nav.Link>
-            <span>Tu</span>
+            <Col >
+              <Nav.Link href="#pricing"> <PersonCircle /> </Nav.Link>
+              <span className='d-none d-md-inline'>Tu</span>
             </Col>
          <Col>
-            <Grid3x3Gap className='fs-5 mt-2'/>
-            <NavDropdown title="Per le aziende"  id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            
+            <Nav.Link href="#pricing"> <Grid3x3Gap/> </Nav.Link>
+            <span className='d-none d-md-inline text-nowrap'>Per le aziende</span>
         </Col>
+      </Row>
           </Nav>
           
-        </Navbar.Collapse>
+       
        
       </Container>
     </Navbar>
