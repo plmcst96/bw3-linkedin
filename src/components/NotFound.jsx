90970192b91393ee29ab1cca46b7cb636e2110ab
Cristina Palmisani <cristina.palmisani@outlook.it>
2023-11-17 16:00:10 +0100

    md

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import svg from "../assets/404.svg"
 import logo from "../assets/link.png"
 
@@ -5,7 +6,14 @@ const PageNotFound = () => {
   return (
     <>
       <div className="cont-404">
-        <img src={logo} alt="" width={"150px"} style={{ marginLeft: "2em" }} />
+        <Link to={"/"}>
+          <img
+            src={logo}
+            alt=""
+            width={"150px"}
+            style={{ marginLeft: "2em" }}
+          />
+        </Link>
         <img src={svg} alt="svg" style={{ marginTop: "5em" }} />
       </div>
     </>
