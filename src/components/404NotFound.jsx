eca85023c315cbb4d4a9919632675fb2923641ea
Cristina Palmisani <cristina.palmisani@outlook.it>
2023-11-17 10:02:30 +0100

    md

---
+++ src/components/404NotFound.jsx
@@ -0,0 +1,15 @@
+import { Col, Container, Row } from "react-bootstrap"
+import logo from "../link.png"
+
+const NotFound = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <img src={logo} alt="" />
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+export default NotFound
