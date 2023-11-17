ebb68091ea8503f7424f65bf5711e8efb280cc1d
Cristina Palmisani <cristina.palmisani@outlook.it>
2023-11-17 15:26:29 +0100

    md

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import NotFound from './components/404NotFound';
+import PageNotFound from './components/404NotFound';
+
+function App() {
+  return (
+    <PageNotFound />
+  );
+}
+
+export default App;
